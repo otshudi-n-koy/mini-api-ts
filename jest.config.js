a10d0eaa3d81de6ec24c2bef6b95209d9387969f
N'Koy OTSHUDI <nkoy.otshudi@proton.me>
2025-12-04 01:43:27 +0100

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          module: 'ESNext',
+          moduleResolution: 'node',
+        },
+      },
+    ],
+  },
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(supertest)/)',
+  ],
+};
