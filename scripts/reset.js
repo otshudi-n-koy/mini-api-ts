eafd662fbe61e5bf1694e2420d573763c46ce268
N'Koy OTSHUDI <nkoy.otshudi@proton.me>
2025-12-04 01:29:05 +0100

    Create reset.js

---
+++ scripts/reset.js
@@ -0,0 +1,23 @@
+import { Pool } from "pg";
+import fs from "fs";
+const sql = fs.readFileSync("scripts/reset.sql", "utf8");
+const pool = new Pool({
+    host: "localhost",
+    port: 5433,
+    user: "miniapiuser",
+    password: "miniapipass",
+    database: "postgres",
+});
+(async () => {
+    try {
+        await pool.query(sql);
+        console.log("✅ Base reset avec succès");
+    }
+    catch (err) {
+        console.error("❌ Erreur lors du reset :", err);
+    }
+    finally {
+        await pool.end();
+    }
+})();
+//# sourceMappingURL=reset.js.map
