00e048cb1b7dc2f99dbd6656f31a6d6fda1dc006
N'Koy OTSHUDI <nkoy.otshudi@proton.me>
2025-12-05 22:49:54 +0100

    Update users.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express';
-import { Pool } from 'pg';
 import type { User } from '../types/user.js';
+import pool from '../db.js';
 
 const router = Router();
-const pool = new Pool(); // config via .env
+
+// Lister les utilisateurs
+router.get('/', async (_req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows as User[]);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error('Erreur SQL :', err.message);
+      res.status(500).json({ error: 'Erreur lors de la récupération' });
+    } else {
+      res.status(500).json({ error: 'Erreur inconnue' });
+    }
+  }
+});
 
 // Ajouter un utilisateur
 router.post('/add', async (req, res) => {
